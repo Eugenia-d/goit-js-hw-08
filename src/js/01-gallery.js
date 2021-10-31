7ab4d4ee3be277771941ca2013a69b60546611cd
Eugenia-d <e.drobiazko@gmail.com>
2021-10-31 18:34:28 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryDivEl = document.querySelector('.gallery');
+const cardsMarkup = createCardsMarkup(galleryItems);
+console.log(galleryDivEl);
+
+galleryDivEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createCardsMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <li>
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li>
+    </div>    
+    `;
+    })
+    .join('');
+}
+galleryDivEl.addEventListener('click', onSelectImage);
+
+function onSelectImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const imgUrl = evt.target.dataset.source;
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
