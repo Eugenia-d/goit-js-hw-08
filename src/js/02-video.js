8d0de76f78d6ffb0fe145d12b26b1397b5fa300b
Eugenia-d <e.drobiazko@gmail.com>
2021-10-31 19:13:32 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import _ from 'lodash.throttle';
 
+var iframe = document.getElementById('vimeo-player');
+var player = new Player(iframe);
+const key = 'videoplayer-current-time';
+player.setCurrentTime(localStorage.getItem(key));
+
+const onTimeUpdate = ({ seconds }) => {
+  localStorage.setItem(key, seconds);
+  console.log(seconds);
+};
+const throtteledOnTimeUpdate = _(onTimeUpdate, 1000);
+
+player.on('timeupdate', throtteledOnTimeUpdate);
